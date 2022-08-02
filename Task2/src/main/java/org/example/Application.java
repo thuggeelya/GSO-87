e450df325761a1b4872dc0a21e0618035fbaaf83
thuggeelya <ilya-fdmdh@mail.ru>
2022-08-02 14:34:42 +0700

    Task2

--- Task2/src/main/java/org/example/Application.java
+++ Task2/src/main/java/org/example/Application.java
@@ -12,20 +12,12 @@ public class Application {
         int nThreads = Integer.parseInt(args[0]);
         Queue<MyRunnable> queue = new LinkedList<>();
         List<MyRunnable> myRunnableList = new ArrayList<>();
-//      List<Thread> threadList = new ArrayList<>();
 
         while (nThread < nThreads) {
             MyRunnable runnable = new MyRunnable(nThread++, System.out, queue);
             myRunnableList.add(runnable);
         }
 
-        myRunnableList.forEach(r -> {
-            Thread thread = new Thread(r);
-            thread.start();
-//          threadList.add(thread);
-        });
-
-//      Thread.sleep(500);
-//      threadList.get(0).interrupt();
+        myRunnableList.forEach(r -> new Thread(r).start());
     }
 }
